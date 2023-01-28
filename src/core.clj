f59d589c24b512892640f5b215ddb65bfa726ab9
wickedev <orange881217@gmail.com>
2023-01-29 05:19:36 +0900

    initial commit

---
+++ src/core.clj
@@ -0,0 +1,249 @@
+(ns core
+  (:require [clojure.java.data :as j]
+            [clojure.walk :as walk]
+            [promesa.core :as p])
+  (:import [com.google.common.base CaseFormat]
+           [com.google.gson GsonBuilder]
+           [graphql GraphQL]
+           [graphql ExecutionInput]
+           [graphql.execution.instrumentation.dataloader DataLoaderDispatcherInstrumentationOptions]
+           [graphql.execution.instrumentation.dataloader DataLoaderDispatcherInstrumentation]
+           [graphql.schema DataFetcher]
+           [graphql.schema.idl SchemaParser]
+           [graphql.schema.idl RuntimeWiring]
+           [graphql.schema.idl SchemaGenerator]
+           [org.dataloader BatchLoaderWithContext]
+           [org.dataloader BatchLoaderContextProvider]
+           [org.dataloader DataLoaderRegistry]
+           [org.dataloader DataLoaderOptions]
+           [org.dataloader DataLoaderFactory]))
+
+(def ^:private gson (.. (GsonBuilder.)
+                        setPrettyPrinting
+                        create))
+
+(defn camelCase->kebab-case [s]
+  (if (string? s)
+    (.to CaseFormat/LOWER_CAMEL
+         CaseFormat/LOWER_HYPHEN s)
+    s))
+
+(def memoize-camelCase->kebab-case
+  (memoize camelCase->kebab-case))
+
+(deftype ^:private UnaryOperatorWrapper [f]
+  java.util.function.UnaryOperator
+  (apply [_ v]
+    (f v)))
+
+(deftype ^:private BatchLoaderWithContextWrapper [f]
+  BatchLoaderWithContext
+  (load [_ keys ctx]
+    (f keys ctx)))
+
+(defrecord ^:private Resolver
+           [qualified-field
+            object-type
+            field
+            option
+            datafetcher
+            batchloader])
+
+(defn- make-batch-arg [{:keys [batch]} ctx args parent]
+  {:ctx (select-keys ctx (:ctx batch))
+   :args (select-keys args (:args batch))
+   :parent (select-keys parent (:parent batch))})
+
+(defn- ctx->m [ctx]
+  (->> (.iterator (.stream ctx))
+       iterator-seq
+       (map (fn [eset]
+              [(.getKey eset)
+               (.getValue eset)]))
+       (into {})))
+
+(defn keywordize-keys
+  "Recursively transforms all map keys from camelCase strings to kebab-case keywords."
+  [m]
+  (let [f (fn [[k v]]
+            (if (string? k)
+              [(-> (memoize-camelCase->kebab-case k)
+                   keyword) v]
+              [k v]))]
+    ;; only apply to maps
+    (walk/postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
+
+(defn- jm->m [jm]
+  (-> (j/from-java jm)
+      keywordize-keys))
+
+(deftype ^:private DataFetcherWrapper [qualified-field option resolve-fn]
+  DataFetcher
+  (get [_this env]
+    (let [dataloader (.getDataLoader env (str qualified-field))
+          ctx (or (ctx->m (.getContext env)) {})
+          args (or (jm->m (.getArguments env)) {})
+          parent (or (jm->m (.getSource env)) {})]
+      (if (and dataloader (:batch option))
+        (.load dataloader (make-batch-arg option ctx args parent))
+        (resolve-fn ctx args parent)))))
+
+(defmulti resolver
+  {:arglists '([& more])}
+  (fn [name & _more] name))
+
+(defmethod resolver :default [_] nil)
+
+(defn- parse-fdecl
+  [fdecl]
+  (let [;; if first fdecl is string? then drop docstring
+        fdecl                  (if (string? (first fdecl))
+                                 (next fdecl)
+                                 fdecl)
+        [qualified-field args] (if (keyword? (first fdecl))
+                                 [(first fdecl) (rest fdecl)]
+                                 [nil fdecl])
+        [option [args & body]] (if (map? (first args))
+                                 [(first args) (rest args)]
+                                 [{} args])]
+    {:qualified-field qualified-field
+     :option          option
+     :args            args
+     :body            body}))
+
+(defn- object-type->fields [object-type]
+  (let [type (.getName object-type)
+        fields (->> (.getFieldDefinitions object-type)
+                    (map #(.getName %)))]
+    (map (fn [field]
+           {:object-type type
+            :field field
+            :qualified-field (keyword (str type "/" field))})
+         fields)))
+
+(defn- type-def-registry->object-types [type-def-registry]
+  (.. type-def-registry
+      types
+      values
+      iterator))
+
+(defn- type-def-registry->fields [type-def-registry]
+  (->> (type-def-registry->object-types type-def-registry)
+       iterator-seq
+       (map object-type->fields)
+       (apply concat)))
+
+(defn- datafetcher-register! [runtime-wiring-builder resolvers]
+  (run!
+   (fn [{:keys [object-type field datafetcher]}]
+     (when (and object-type field datafetcher)
+       (.. runtime-wiring-builder
+           (type
+            object-type
+            (->UnaryOperatorWrapper
+             (fn [builder]
+               (.dataFetcher builder field datafetcher)))))))
+   resolvers))
+
+
+(defn- build-runtime-wiring [resolvers]
+  (let [runtime-wiring-builder (RuntimeWiring/newRuntimeWiring)]
+    (datafetcher-register!
+     runtime-wiring-builder
+     resolvers)
+    (.build runtime-wiring-builder)))
+
+(defn- build-resolvers [fields]
+  (map (fn [{:keys [qualified-field]}]
+         (resolver qualified-field)) fields))
+
+(defn- build-schema [type-def-registry resolvers]
+  (let [schema-generator (SchemaGenerator.)
+        runtime-wiring (build-runtime-wiring resolvers)]
+    (.. schema-generator
+        (makeExecutableSchema
+         type-def-registry
+         runtime-wiring))))
+
+(defn- build-dataloader-registry [resolvers loader-options]
+  (let [registry (DataLoaderRegistry.)]
+    (run! (fn [{:keys [qualified-field batchloader] :as _resolver}]
+            (let [dataloader (DataLoaderFactory/newDataLoader
+                              batchloader
+                              loader-options)]
+              (.register registry
+                         (str qualified-field)
+                         dataloader)))
+          resolvers)
+    registry))
+
+(defmacro defresolver
+  {:arglists '([qualified-field args body]
+               [qualified-field docstring|options args body]
+               [qualified-field docstring options args body])}
+  ([& fdecl]
+   (let [{:keys [qualified-field option args body]} (parse-fdecl fdecl)
+         {:keys [batch]} option
+         [_ object-type field] (re-matches
+                                #":(\w+)/(\w+)"
+                                (str qualified-field))]
+     `(defmethod resolver
+        ~qualified-field
+        [_#]
+        (let [resovler-fn# (fn ~args ~@body)
+              datafetcher# (->DataFetcherWrapper
+                            ~qualified-field
+                            ~option
+                            resovler-fn#)
+              batchloader# (when
+                            ~batch
+                             (->BatchLoaderWithContextWrapper
+                              (fn [keys# ctx#]
+                                (p/create
+                                 (fn [resolve# reject#]
+                                   (try
+                                     (let [results# (resovler-fn#
+                                                     (.getContext ctx#)
+                                                     keys#)]
+                                       (-> results#
+                                           walk/stringify-keys
+                                           resolve#))
+                                     (catch Exception e#
+                                       (reject# e#))))))))]
+          (->Resolver ~qualified-field
+                      ~object-type
+                      ~field
+                      ~option
+                      datafetcher#
+                      batchloader#))))))
+
+(defn build-prepared-schema [sdl]
+  (let [parser (SchemaParser.)
+        type-def-registry (.parse parser sdl)
+        resolvers (-> type-def-registry
+                      type-def-registry->fields
+                      build-resolvers)
+        schema (build-schema type-def-registry resolvers)]
+    {:schema schema
+     :resolvers resolvers}))
+
+(defn execute-query [{:keys [schema resolvers]} query ctx]
+  (let [context-provider (reify BatchLoaderContextProvider
+                           (getContext [_this] ctx))
+        dispatcher-options (.. (DataLoaderDispatcherInstrumentationOptions/newOptions)
+                               (includeStatistics true))
+        loader-options  (.. (DataLoaderOptions/newOptions)
+                            (setCachingEnabled false)
+                            (setBatchLoaderContextProvider context-provider))
+        registry (build-dataloader-registry resolvers loader-options)
+        dispatcher-instrumentation (DataLoaderDispatcherInstrumentation. dispatcher-options)
+        executable (.. (GraphQL/newGraphQL schema)
+                       (instrumentation dispatcher-instrumentation)
+                       build)
+        input (.. (ExecutionInput/newExecutionInput)
+                  (query query)
+                  (dataLoaderRegistry registry)
+                  (graphQLContext ctx)
+                  build)
+        result (.execute executable input)]
+    (.toJson gson result)))
---
+++ src/user.clj
@@ -0,0 +1,73 @@
+(ns user
+  (:require [clojure.java.io :as io]
+            [core :refer [build-prepared-schema defresolver execute-query]]))
+
+(defresolver :Query/hello
+  {:batch {:args [:input-num]}}
+  [ctx batch-args]
+  (prn :Query/hello :ctx ctx :batch-args batch-args)
+  (map str batch-args))
+
+(defresolver :Query/hi
+  [ctx args parent]
+  (prn :Query/hi :ctx ctx :args args :parent parent)
+  "greeting")
+
+(defresolver :Query/nested
+  {:batch {}}
+  [ctx batch-args]
+  (prn :Query/nested ctx batch-args)
+  (map (fn [_] {:id (random-uuid)}) batch-args))
+
+(defresolver :Nested/nested
+  {:batch {:parent [:id]}}
+  [ctx batch-args]
+  (prn :Nested/nested ctx batch-args)
+  (map (fn [_] {:id (random-uuid)}) batch-args))
+
+(defresolver :Nested/nesteds
+  {:batch {:parent [:id]}}
+  [ctx batch-args]
+  (prn :Nested/nesteds ctx batch-args)
+  (map (fn [_] [{:id (random-uuid)} {:id (random-uuid)}]) batch-args))
+
+(def prepared-schema (-> "schema.graphqls"
+                         io/resource
+                         slurp
+                         build-prepared-schema))
+
+(comment
+  (printf
+   (execute-query
+    prepared-schema
+    "query {
+      aHi: hi
+      bHi: hi
+      aHello: hello(inputNum: 1)
+      bHello: hello(inputNum: 2)
+      cHello: hello(inputNum: 2)
+      dHello: hello(inputNum: 2)
+   }"
+    {:foo 1}))
+
+  (printf
+   (execute-query
+    prepared-schema
+    "query {
+      nested {
+        id
+        nested {
+          id
+        }
+        nesteds {
+          id
+          nested {
+            id
+            nested {
+              id
+            }
+          }
+        }
+      }
+    }"
+    {:bar 1})))
